43e506efc054760e916c7fc98564c6dc374ed850
behrooz3500 <behrooz3500@yahoo.com>
2025-10-06 09:52:40 +0330

    uses c sharp as the stimulsoft report scripting language
    -> ReportEngine

--- ReportGenerator/ReportEngine.cs
+++ ReportGenerator/ReportEngine.cs
@@ -24,6 +24,7 @@ namespace ReportGenerator
         {
             try
             {
+                _stiReport.ScriptLanguage = StiReportLanguageType.CSharp;
                 _stiReport.Load(templatePath);
             }
             catch (Exception ex)
